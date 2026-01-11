65807bb9badae60defb8cd4224d3921427c77a83
Just-a-lurker <111871642+Just-a-lurker@users.noreply.github.com>
2026-01-11 18:00:56 +0700

    update clamper

--- clamp.py
+++ clamp.py
@@ -61,32 +61,67 @@ def shift_f0_contour(
     f0,
     HZ_MIN,
     HZ_MAX,
-    max_semitone=5
+    max_semitone=5,
+    safety_semitone=2
 ):
-    #f0 = f0.astype(np.float32, copy=False)
-    voiced = f0 > 0
+    f0 = f0.astype(np.float32, copy=False)
+    voiced = f0 > 65
     if not np.any(voiced):
         return f0
 
     med = np.median(f0[voiced])
     semitone = 0.0
+    high = f0 > HZ_MAX
+    low = (f0 < HZ_MIN) & voiced
 
-    if med < HZ_MIN:
-        semitone = 12 * np.log2(HZ_MIN / med)
-    elif med > HZ_MAX:
-        semitone = 12 * np.log2(HZ_MAX / med)
+    # if med < HZ_MIN:
+    #     print("LOW:")
+    #     mean_low = np.mean(f0[low])
+    #     print(mean_low)
+    #     semitone = 12 * np.log2(HZ_MIN / mean_low)
+    # elif med > HZ_MAX:
+    #     print("HIGH:")
+    #     mean_high = np.mean(f0[high])
+    #     print(mean_high)
+    #     semitone = 12 * np.log2(HZ_MAX / mean_high)
+    high_mean = np.mean(f0[high])
+    low_mean = np.mean(f0[low])
+    semitone_low = 12 * np.log2(HZ_MIN / low_mean)
+    semitone_high = 12 * np.log2(HZ_MAX / high_mean)
+    semitone = semitone_low + semitone_high
 
     # clamp theo khả năng extrapolate của model
     semitone = np.clip(semitone, -max_semitone, max_semitone)
-    semitone = 0.8
     print(semitone)
+    print(semitone_high)
+    print(semitone_low)
     print(med)
     print(HZ_MIN)
-    print(HZ_MIN*0.8)
     print(HZ_MAX)
 
     if abs(semitone) < 0.1:
         return f0
 
     f0[voiced] *= 2 ** (semitone / 12)
+
+    # high = f0 > HZ_MAX
+    #
+    # if np.any(high):
+    #     mean_high = np.mean(f0[high])
+    #     print(mean_high)
+    #     corr_semitone = 12 * np.log2(HZ_MAX / mean_high)
+    #     f0[high] *= 2 ** (corr_semitone / 12)
+    #     print(corr_semitone)
+    #
+    # # lower side
+    # hz_min_limit = HZ_MIN * 2 ** (-safety_semitone / 12)
+    # low = (f0 < hz_min_limit) & voiced
+    #
+    # if np.any(low):
+    #     mean_low = np.mean(f0[low])
+    #     corr_semitone = 12 * np.log2(HZ_MIN / mean_low)
+    #     f0[low] *= 2 ** (corr_semitone / 12)
+    #     print(corr_semitone)
+
+
     return f0
